9f7e3ea64ae4dfc409784a6b6d887540a7d3444c
vjo <vjorlikowski@users.noreply.github.com>
2011-04-21 21:47:10 +0000

    Fix libtool generation problem

--- bittorrent_jni/prep_build.sh
+++ bittorrent_jni/prep_build.sh
@@ -34,6 +34,10 @@ mv transmission-$TRANSMISSION_VERSION/m4 ..
 
 cd ..
 
+cd m4
+rm libtool.m4 ltoptions.m4 ltsugar.m4 lt~obsolete.m4 ltversion.m4
+cd ..
+
 echo
 echo "Patching and running autoreconf procedures..."
 patch -p1 < transmission_find_port.patch
